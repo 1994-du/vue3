717557006205b090fdfc549bf9a28f906f9eadcd
du_1994 <1746215067@qq.com>
2023-05-21 01:46:46 +0800

    9999

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
 const Axios = axios.create({
     timeout:3000
 })
@@ -7,6 +8,10 @@ Axios.interceptors.request.use(req=>{
     return req
 })
 Axios.interceptors.response.use(res=>{
-    return res
+    ElMessage({
+        message:res.data.msg,
+        type:res.data.status
+    })
+    return res.data
 })
 export default Axios;
--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     port: '3000',
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/',
+        target: 'http://localhost:1234/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '') // 不可以省略rewrite
       }

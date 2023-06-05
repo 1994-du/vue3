dd685c44efe8bac411b6e29437f426a70c608889
du_1994 <1746215067@qq.com>
2023-06-05 11:12:18 +0800

    20230605

--- src/views/websocket.vue
+++ src/views/websocket.vue
@@ -1,9 +1,10 @@
 <script setup>
 import {io} from "socket.io-client"
-import { onMounted,ref,reactive, onDeactivated, onUnmounted, onActivated } from "vue";
+import { onMounted,ref,reactive, onDeactivated, onUnmounted, onActivated,nextTick } from "vue";
 const chatList = ref([])
 const chatMsg = ref('')
 let socket=ref(null)
+// 获取服务器时间
 const currentTime = function(){
   let xmlHttp = new XMLHttpRequest()
   if(!xmlHttp){
@@ -17,31 +18,37 @@ const currentTime = function(){
   console.log('time',time);
   return serverTime.toLocaleString()
 }
+let userId = JSON.parse(localStorage.getItem('token')).id
 onMounted(()=>{
-    socket = io('http://192.168.31.65:1234');
+    socket = io('http://localhost:1234');
     socket.on('connect',()=>{
         console.log(socket.id,'监听客户端连接成功-connect');
     })
-
-
-    
-
     socket.on('message',msg=>{
-      chatList.value.push(msg)
       console.log('服务器发送信息',msg);
+      chatList.value.push(msg)
+      to_footer()
+      
     })
     socket.on('msg_res',(msg)=>{
       console.log('其他客户端信息',msg);
       chatList.value.push(msg)
+      to_footer()
     })
     socket.on('disconnect',()=>{
       socket.disconnect()
       socket.close()
     })
 })
-onActivated(()=>{
-  console.log('actived');
-})
+ /*滚动条到底部*/
+const to_footer = function() {
+  nextTick(()=>{
+    var div = document.getElementsByClassName('chat-content')[0];
+    console.log('scrollHeight',div.scrollHeight);
+    div.scrollTop = div.scrollHeight;
+  })
+    
+}
 // 发送消息
 const sendMsg = ()=>{
   let userInfo=JSON.parse(localStorage.getItem('token'))
@@ -55,40 +62,40 @@ onUnmounted(()=>{
   socket.close()
   socket=null
 })
-onDeactivated(()=>{
-  console.log('onDeactivated');
-})
 </script>
 <template>
 <div class="container">
     <div class="chat-content">
-      <!-- <img :src="avatar2" alt=""> -->
       <template v-if="chatList && chatList.length">
         <div
           v-for="(chat, index) in chatList"
           class="message-box"
           :key="index"
         >
-          <div class="user">
-            <el-avatar class="avatar" :src="chat.avatar" ></el-avatar>
+          <div class="user" :class="chat.id===userId?'isSelf':''">
+            <el-avatar class="avatar" :src="chat.avatar" shape="square"></el-avatar>
             <div class="info">
               <div class="name">{{chat.name}}</div>
-              <div class="time">{{ chat.time }}</div>
+              <div class="message"><div class="block">{{chat.msg}}</div></div>
+              <!-- <div class="time">{{ chat.time }}</div> -->
             </div>
             <!-- <el-avatar v-if="chat.id === userInfo.id" class="avatar" :src="chat.avatar" ></el-avatar> -->
           </div>
-          <div class="message"><div class="block">{{chat.msg}}</div></div>
+          
         </div>
       </template>
-      <div v-else class="empty">
-        没有消息
-      </div>
+      <template v-else>
+        <div class="empty">
+          没有消息
+        </div>
+      </template>
     </div>
     <div class="chat-bottom">
-      <el-input v-model="chatMsg" class="chat-input" placeholder="请输入内容" />
+      <el-input v-model="chatMsg" class="chat-input" placeholder="请输入内容" @keydown.enter="sendMsg"/>
       <el-button class="chat-btn" type="primary" @click="sendMsg">发送</el-button>
     </div>
   </div>
 </template>
 <style lang="less" scoped>
+@import "@/styles/websocket.less";
 </style>

2c80b0f0ef1a273d563e22791806d7f0063f2c23
du_1994 <1746215067@qq.com>
2024-07-03 10:48:09 +0800

    pl

--- src/views/mysql.vue
+++ src/views/mysql.vue
@@ -4,6 +4,7 @@
     <p>断开连接：\q </p>
     <p>命令行启动mysql：mysql服务安装路径下 管理员开打cmd， net start mysql</p>
     <p>命令行关闭mysql：mysql服务安装路径下 管理员开打cmd， net stop mysql</p>
+    <p>修改密码：ALTER USER 'username'@'localhost' IDENTIFIED BY 'new_password';</p>
 
 </template>
 <style lang="scss" scoped>

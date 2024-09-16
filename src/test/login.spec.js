ccf96a469f136326d24b93402c0f017d620e478c
du_1994 <1746215067@qq.com>
2024-09-16 14:11:56 +0800

    玩玩

--- src/test/login.spec.js
+++ src/test/login.spec.js
@@ -4,9 +4,7 @@ import { ElButton, ElInput } from 'element-plus'
 import { describe, it, expect, vi } from 'vitest';
 import axios from 'axios';
 import { toLogin ,toRegistry} from "@/api/api";
-
-
-
+// 缺少mock请求
 describe('Login.vue', () => {
   it('fetches data on mount', async () => {
     const wrapper = mount(Login,{

bea43061e7dd47599f54859bb3d2724fd1cdee61
du_1994 <1746215067@qq.com>
2024-09-16 12:16:49 +0800

    单元测试mock

--- src/test/login.spec.js
+++ src/test/login.spec.js
@@ -1,45 +1,23 @@
 import { mount } from '@vue/test-utils'
 import Login from '../Login.vue'
-import { ElButton } from 'element-plus'
+import { ElButton, ElInput } from 'element-plus'
 import { describe, it, expect, vi } from 'vitest';
 import axios from 'axios';
 import { toLogin ,toRegistry} from "@/api/api";
 
-vi.mock('axios', () => {
-    const mockAxiosInstance = {
-        interceptors: {
-          request: {
-            use: vi.fn((resolve) => resolve({ data: { status: true, data: 'mocked data' } })),
-          },
-          response: {
-            use: vi.fn((resolve) => resolve({ data: { status: true, data: 'mocked data' } })),
-          },
-        },
-        get: vi.fn(() => Promise.resolve({ data:{
-            status:'success'
-        } })),
-        post: vi.fn(() => Promise.resolve({ data:{
-            status:'success'
-        } })),
-      };
-    
-      return {
-        default: {
-          create: vi.fn(() => mockAxiosInstance),
-        },
-      };
-});
+
 
 describe('Login.vue', () => {
   it('fetches data on mount', async () => {
     const wrapper = mount(Login,{
         global: {
             components: {
-                ElButton
+                ElButton,
+                ElInput
             }
         }
     })
-    await wrapper.vm.$nextTick();
+    // await wrapper.vm.$nextTick();
     expect(wrapper.text()).toContain('登录');
   });
 });
--- vitest.config.js
+++ vitest.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom', // 使用 jsdom 模拟浏览器环境
-    // setupFiles: './tests/setup.js', // 如果需要初始化配置，可以在这里设置路径
+    setupFiles: './src/test/vitest.setup.js', // 如果需要初始化配置，可以在这里设置路径
     coverage: {
       provider: 'v8',  // 使用 c8 生成覆盖率报告
       reporter: ['text', 'html'],  // 生成 HTML 和 text 格式的报告

67292f2615944a7be1274f605e878be6260f8418
du_1994 <1746215067@qq.com>
2025-03-12 01:34:19 +0800

    呢

---
+++ src/views/Js/class.vue
@@ -0,0 +1,29 @@
+<template>
+    <h4>Class</h4>
+    <h5>静态属性</h5>
+    <code>
+        <pre>使用类名访问</pre>
+        <pre>所有实例共享此属性</pre>
+    </code>
+    <h5>私有属性</h5>
+    <code>
+        <pre>在类内部访问</pre>
+        <pre>每个实例独有此属性</pre>
+    </code>
+</template>
+
+<script setup>
+class BankAccount {
+  #balance = 1000; // 私有属性
+
+  getBalance() {
+      return this.#balance; // ✅ 类内部可以访问
+  }
+}
+
+const account = new BankAccount();
+console.log(account.getBalance()); // ✅ 1000
+console.log(BankAccount.#balance); // ❌ 报错 (SyntaxError: Private field '#balance' must be declared in an enclosing class)
+</script>
+<style scoped lang='less'>
+</style>

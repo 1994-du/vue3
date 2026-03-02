8015d60f79890d9ea01d2dfaabe93d7ef6610c8f
du_1994 <1746215067@qq.com>
2026-03-02 18:52:24 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineConfig(({mode,command}) => {
   const env = loadEnv(mode,process.cwd())
   return{
